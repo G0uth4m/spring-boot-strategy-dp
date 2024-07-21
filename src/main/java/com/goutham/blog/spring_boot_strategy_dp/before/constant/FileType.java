07fed299b3b9bd64f42b13ce6909c719ea90580f
g0uth4m <goutham67r@gmail.com>
2024-07-21 18:47:11 +0530

    use constants for file type

---
+++ src/main/java/com/goutham/blog/spring_boot_strategy_dp/before/constant/FileType.java
@@ -0,0 +1,7 @@
+package com.goutham.blog.spring_boot_strategy_dp.before.constant;
+
+public class FileType {
+    public static final String CSV = "CSV";
+    public static final String XML = "XML";
+    public static final String JSON = "JSON";
+}
--- src/main/java/com/goutham/blog/spring_boot_strategy_dp/before/service/FileParserService.java
+++ src/main/java/com/goutham/blog/spring_boot_strategy_dp/before/service/FileParserService.java
@@ -1,5 +1,6 @@
 package com.goutham.blog.spring_boot_strategy_dp.before.service;
 
+import com.goutham.blog.spring_boot_strategy_dp.before.constant.FileType;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -9,17 +10,17 @@ import java.util.Objects;
 public class FileParserService {
 
     public void parse(File file, String fileType) {
-        if (Objects.equals(fileType, "CSV")) {
+        if (Objects.equals(fileType, FileType.CSV)) {
             /*
             a huge implementation to parse CSV file
             and persist data in db
              */
-        } else if (Objects.equals(fileType, "JSON")) {
+        } else if (Objects.equals(fileType, FileType.JSON)) {
             /*
             a huge implementation to parse JSON file
             and persist data in db
              */
-        } else if (Objects.equals(fileType, "XML")) {
+        } else if (Objects.equals(fileType, FileType.XML)) {
             /*
             a huge implementation to parse XML file
             and persist data in db
